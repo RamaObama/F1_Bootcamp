5c6af134590c72b1808a8ae86b873941ae28583f
Rasuli Ramin [deltajed] <i@rrmstu.ru>
2022-07-23 13:13:45 +0300

    [fix] remove bootsrap source css style

--- Day07/day_07/src/App.js
+++ Day07/day_07/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import StudentInfo from "./components/studentInfo";
-import StopWatch from "./components/stopWatch.";
+import StopWatch from "./components/stopWatch";
 import {useState} from "react";
 
 function App() {
